c9d50cabc5e9d9ed91e4c0ecd2de372f1e83c555
CUONGLMGC60543\Cuong Xay Lua <lamanhcuong1996@gmail.com>
2018-04-12 17:00:55 +0700

    full

--- AdminWebSite/Controllers/OrderDetailController.cs
+++ AdminWebSite/Controllers/OrderDetailController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
@@ -10,86 +11,19 @@ using System.Web.Mvc;
 using AdminWebSite.Models;
 using Microsoft.Reporting.WebForms;
 
+
 namespace AdminWebSite.Controllers
 {
     public class OrderDetailController : Controller
     {
         private DBWatchEntities db = new DBWatchEntities();
-
         // GET: OrderDetail
         public ActionResult Reports()
         {
             var orderDetail = db.OrderDetail.Include(o => o.Order).Include(o => o.Product);
             return View(orderDetail.ToList());
-        }
+        }      
 
-        //report
-        public ActionResult XuatReport(String id)
-        {
-            LocalReport lr = new LocalReport();
-            string path = Path.Combine(Server.MapPath("~/Report"), "Report.rdlc");
-            if (System.IO.File.Exists(path))
-            {
-                lr.ReportPath = path;
-            }
-            else
-            {
-                return View();
-            }
-
-
-            List<OrderDetail> cm = new List<OrderDetail>();
-
-
-            using (DBWatchEntities db = new DBWatchEntities())
-            {
-
-                //var list = (from I in db.OrderDetail
-                //            where I.Order.Customer.CusName == id
-                //            orderby I.Order.Date
-                //            select new { I.Order.OrderID, I.Product.ProName, I.Quantity, I.Price }).FirstOrDefault();
-
-
-                // var a = list.ToString();
-                cm = db.OrderDetail.ToList();
-                ReportDataSource rd = new ReportDataSource("MyDataSet", cm);
-                lr.DataSources.Add(rd);
-                string reportType = id;
-                string mineType;
-                string encoding;
-                string fileNameExtension;
-
-                string deviceInfo =
-                    "<DeviceInfo>" +
-                    "<OutputFormat>" + id + "</OutputFormat>" +
-                    "<PageWidth>8.5in</PageWidth>" +
-                    "<PageHeight>11in</PageHeight>" +
-                    "<MarginTop>0.5in</MarginTop>" +
-                    "<MarginLeft>1in</MarginLeft>" +
-                    "<MarginRight>1in</MarginRight>" +
-                    "<MarginBottom>0.5in</MarginBottom>" +
-                    "</DeviceInfo>"
-                    ;
-                Warning[] warnings;
-                string[] streams;
-                byte[] rendereBytes;
-
-                rendereBytes = lr.Render
-                    (
-                        reportType,
-                        deviceInfo,
-                        out mineType,
-                        out encoding,
-                        out fileNameExtension,
-                        out streams,
-                        out warnings
-                    );
-                return File(rendereBytes, mineType);
-            }
-
-
-
-        }
 
         protected override void Dispose(bool disposing)
         {
